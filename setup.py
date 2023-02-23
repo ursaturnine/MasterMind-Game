95dc4cdf285f6eb98759d177cb5a75df3e3c1236
Tyrah <tyrah96@gmail.com>
2023-02-22 18:27:39 -0800

    patch; removed requests package install from set up file; doesn't install

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="Master-Mind-Game",
-    version="1.1.0",
+    version="1.1.1",
     description="MasterMindGame using Click for an interactive CLI GUI",
     author="Tyrah Gullette",
     author_email="tyrah96@gmail.com",
     url="https://github.com/ursaturnine/MasterMind-Game",
     packages=find_packages(exclude=["tests*", "testing*"]),
     install_package_data=True,
-    install_requires=["click", "requests", "copy", "pytest", "random"],
+    install_requires=["click","copy", "pytest", "random"],
     entry_points={
         'console_scripts': [
             'game = src.optionsMenu:menu'
